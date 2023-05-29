17ff6adab49c7ecf1322ec0c33d2019c40270951
mackliu007 <mackliu007@163.com>
2023-05-29 10:44:10 +0800

    flink1.13.1

---
+++ src/main/java/org/apache/flink/connector/elasticsearch/source/ESDynamicTableSource.java
@@ -0,0 +1,67 @@
+package org.apache.flink.connector.elasticsearch.source;
+
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.table.connector.ChangelogMode;
+import org.apache.flink.table.connector.format.DecodingFormat;
+import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.connector.source.ScanTableSource;
+import org.apache.flink.table.connector.source.SourceFunctionProvider;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.types.DataType;
+
+/**
+ * @ClassName: ESDynamicTableSource
+ * @Description: ESDynamicTableSource
+ * @Author: lyx
+ * @Date: 2023/5/26 11:22
+ */
+public class ESDynamicTableSource implements ScanTableSource {
+
+    private final String hosts;
+    private final String username;
+    private final String password;
+    private final String index;
+    private final String document_type;
+    private Integer fetch_size;
+    //...
+    private final DecodingFormat<DeserializationSchema<RowData>> decodingFormat;
+    private final DataType producedDataType;
+
+    public ESDynamicTableSource(String hosts, String username, String password, String index, String document_type, Integer fetch_size, DecodingFormat<DeserializationSchema<RowData>> decodingFormat, DataType producedDataType) {
+        this.hosts = hosts;
+        this.username = username;
+        this.password = password;
+        this.index = index;
+        this.document_type = document_type;
+        this.fetch_size = fetch_size;
+        this.decodingFormat = decodingFormat;
+        this.producedDataType = producedDataType;
+    }
+
+    @Override
+    public ChangelogMode getChangelogMode() {
+        return decodingFormat.getChangelogMode();
+    }
+
+    @Override
+    public ScanRuntimeProvider getScanRuntimeProvider(ScanContext runtimeProviderContext) {
+        final DeserializationSchema<RowData> deserializer = decodingFormat.createRuntimeDecoder(
+                runtimeProviderContext,
+                producedDataType);
+
+        final ESSourceFunction sourceFunction = new ESSourceFunction(
+                hosts, username, password,index,document_type,fetch_size,deserializer);
+
+        return SourceFunctionProvider.of(sourceFunction, true);
+    }
+
+    @Override
+    public DynamicTableSource copy() {
+        return new ESDynamicTableSource(hosts, username, password,index,document_type,fetch_size,decodingFormat,producedDataType);
+    }
+
+    @Override
+    public String asSummaryString() {
+        return "elastic Table Source";
+    }
+}
---
+++ src/main/java/org/apache/flink/connector/elasticsearch/source/ESSqlFactory.java
@@ -0,0 +1,95 @@
+package org.apache.flink.connector.elasticsearch.source;
+
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.configuration.ConfigOption;
+import org.apache.flink.configuration.ConfigOptions;
+import org.apache.flink.configuration.ReadableConfig;
+import org.apache.flink.table.connector.format.DecodingFormat;
+import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.factories.DeserializationFormatFactory;
+import org.apache.flink.table.factories.DynamicTableSourceFactory;
+import org.apache.flink.table.factories.FactoryUtil;
+import org.apache.flink.table.types.DataType;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.apache.flink.table.factories.FactoryUtil.FORMAT_SUFFIX;
+
+/**
+ * @ClassName: ESSqlFactory
+ * @Description: ESSqlFactory
+ * @Author: lyx
+ * @Date: 2023/5/26 11:18
+ * @Copyright: Fujian Linewell Software Co., Ltd. All rights reserved.
+ * 注意：本内容仅限于福建南威软件股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+public class ESSqlFactory implements DynamicTableSourceFactory {
+    public static final ConfigOption<String> HOSTS= ConfigOptions.key("hosts").stringType().noDefaultValue();
+    public static final ConfigOption<String> USERNAME = ConfigOptions.key("username").stringType().noDefaultValue();
+    public static final ConfigOption<String> PASSWORD = ConfigOptions.key("password").stringType().noDefaultValue();
+    public static final ConfigOption<String> INDEX = ConfigOptions.key("index").stringType().noDefaultValue();
+    public static final ConfigOption<String> DOCUMENT_TYPE = ConfigOptions.key("document_type").stringType().noDefaultValue();
+    public static final ConfigOption<String> FORMAT = ConfigOptions.key("format").stringType().noDefaultValue();
+    public static final ConfigOption<Integer> FETCH_SIZE = ConfigOptions.key("fetch_size").intType().noDefaultValue();
+
+
+    public static final ConfigOption<String> VALUE_FORMAT =
+            ConfigOptions.key("value" + FORMAT_SUFFIX).stringType().noDefaultValue();
+    @Override
+    public String factoryIdentifier() {
+        return "elasticsearch7-source";
+    }
+
+    @Override
+    public Set<ConfigOption<?>> requiredOptions() {
+        final Set<ConfigOption<?>> options = new HashSet<>();
+        options.add(HOSTS);
+        options.add(INDEX);
+        options.add(USERNAME);
+        options.add(PASSWORD);
+        options.add(FactoryUtil.FORMAT);
+        // use pre-defined option for format
+        return options;
+    }
+
+    @Override
+    public Set<ConfigOption<?>> optionalOptions() {
+        final Set<ConfigOption<?>> options = new HashSet<>();
+        options.add(FORMAT);
+        options.add(FETCH_SIZE);
+        options.add(DOCUMENT_TYPE);
+        return options;
+    }
+
+    @Override
+    public DynamicTableSource createDynamicTableSource(Context context) {
+        final FactoryUtil.TableFactoryHelper helper = FactoryUtil.createTableFactoryHelper(this, context);
+        // 获取解码器
+        final DecodingFormat<DeserializationSchema<RowData>> valueFormat =
+                (DecodingFormat)helper.discoverOptionalDecodingFormat(
+                        DeserializationFormatFactory.class, FactoryUtil.FORMAT).orElseGet(() -> {
+                    return helper.discoverDecodingFormat(DeserializationFormatFactory.class, VALUE_FORMAT);
+                });
+
+        final DecodingFormat<DeserializationSchema<RowData>> decodingFormat = helper.discoverDecodingFormat(
+                DeserializationFormatFactory.class, FactoryUtil.FORMAT);
+
+        helper.validate();
+
+        final ReadableConfig options = helper.getOptions();
+        final String hosts = options.get(HOSTS);
+        final String username = options.get(USERNAME);
+        final String password = options.get(PASSWORD);
+        final String index = options.get(INDEX);
+        final String document_type = options.get(DOCUMENT_TYPE);
+        Integer fetch_size = options.get(FETCH_SIZE);
+        //...
+
+        final DataType producedDataType = context.getCatalogTable().getSchema().toPersistedRowDataType();
+
+        return new ESDynamicTableSource(hosts, username, password,index,document_type,fetch_size,valueFormat, producedDataType);
+
+    }
+}
